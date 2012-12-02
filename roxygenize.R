a57274e34a2dd00fbe316034321084c617cf33db
bbuchsbaum (Dr. ) <bbuchsbaum@rotman-baycrest.on.ca>
2012-12-02 21:08:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/neuroim@109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- roxygenize.R
+++ roxygenize.R
@@ -3,10 +3,24 @@
 library(methods)
 
 
-
-library(roxygen)
+options(useFancyQuotes = FALSE)
+library(roxygen2)
 roxygenize("pkg",
  roxygen.dir="pkg",
  copy.package=FALSE,
  unlink.target=FALSE,
- use.Rd2=TRUE)
+ overwrite=TRUE)
+ #use.Rd2=TRUE)
+
+library(roxygen3)
+roxygenise("pkg")
+ #use.Rd2=TRUE)
+
+#setwd("pkg/man")
+
+#lapply(list.files(".", ".*Rd"), function(fname) {
+#	system(paste("sed s/[”“]/'\"'/g", fname, " > tmp"))
+#	system(paste("mv tmp", fname))
+#})
+
+#setwd("../..")
